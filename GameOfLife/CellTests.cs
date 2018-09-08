ba61ee576a524c6759fc6c0f250e770fd738286f
iggywiggy <pipelynn82>
2018-09-08 16:29:58 +0100

    Add project files.

---
+++ GameOfLife/CellTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GameOfLife
+{
+    public class CellTests
+    {
+        [Test]
+        public void Cell_IsAlive()
+        {
+            var cell = new Cell
+            {
+                IsAlive = true
+            };
+
+            var result = cell.IsAlive;
+
+            Assert.That(result, Is.True);
+        }
+    }
+
+    public class Cell
+    {
+        public bool IsAlive;
+    }
+}
